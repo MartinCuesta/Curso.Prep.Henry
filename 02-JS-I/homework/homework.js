5eda1eee3152f25dbaa9c4d3c37f68714ec82acb
martin cuesta <martin.quaker@gmail.com>
2022-08-11 23:46:21 -0300

    littel fix

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -102,7 +102,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % === 0) {
+  if (num % == 0) {
     return true;
   }
   return false;
