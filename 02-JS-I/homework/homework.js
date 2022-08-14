8327376077ccbcb8fa4db6d6661cb44fc06ff9ce
martin cuesta <martin.quaker@gmail.com>
2022-08-13 22:15:14 -0300

    homework 1.4

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,9 +226,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
- if (letra.lenth > 1) {
+ 
+ letra.toLowerCase()
+
+  if (letra.length > 1) {
     return "Dato incorrecto";
- }else if (letra = "a" || "e" || "i" || "o" || "u") {
+ }else if (letra == "a" || "e" || "i" || "o" || "u") {
     return "Es vocal"; 
  }else {
     return "Dato incorrecto";
